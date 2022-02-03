3611a9cc4eb00b3566bda8b8b87d8d4a6240684f
pogopolice <pogopolice@gmail.com>
2022-02-03 09:03:08 -0500

    use a function expression

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+const start = function() {
   console.log('Game is starting....')
-}
+};
 
 // const person = {
+  // name: 'Max',
   // greet: function greet() {
     // console.log('Hello there!');
   // }
 // };
-// 
+
 // person.greet();
 
-startGameBtn.addEventListener('click', startGame);
+// console.dir(startGame);
+
+startGameBtn.addEventListener('click', start);
