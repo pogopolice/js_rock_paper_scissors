21db37912468556900c1e5d3c911af2c92797db1
pogopolice <pogopolice@gmail.com>
2022-02-14 09:13:07 -0500

    set the default argument

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const getPlayerChoice = () => {
   ).toUpperCase();
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-    return DEFAULT_USER_CHOICE;
+    return;
   }
   return selection;
 };
@@ -33,7 +33,10 @@ const getComputerChoice = () => {
   }
 };
 
-const getWinner = (cChoice, pChoice) =>
+const getWinner = (
+  cChoice,
+  pChoice = DEFAULT_USER_CHOICE
+  ) =>
   cChoice === pChoice
     ? RESULT_DRAW
     : (cChoice === ROCK && pChoice == PAPER) ||
@@ -62,8 +65,13 @@ startGameBtn.addEventListener('click', () => {
   console.log('Game is starting...');
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
-  const winner = getWinner(computerChoice, playerChoice);
-  let message = `You picked ${playerChoice}, \ncomputer picked ${computerChoice}, \ntherefore you `;
+  let winner;
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice);
+  } else {
+    winner = getWinner(computerChoice);
+  }
+  let message = `You picked ${playerChoice ? playerChoice : DEFAULT_USER_CHOICE}, \ncomputer picked ${computerChoice}, \ntherefore you `;
   if (winner === RESULT_DRAW) {
     message = message + 'had a draw';
   } else if (winner === RESULT_PLAYER_WINS) {
