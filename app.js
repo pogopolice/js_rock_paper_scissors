ca4a2c8db44621fc5de64bf69c5b9e9b03b9cce4
pogopolice <pogopolice@gmail.com>
2022-02-16 09:26:26 -0500

    use a callback function to display the sum in an alert

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ startGameBtn.addEventListener('click', () => {
 
 // not related to game
 
-const sumUp = (...numbers) => {
+const sumUp = (resultHandler, ...numbers) => {
   const validateNumber = (number) => {
     return isNaN(number) ? 0 : number;
   };
@@ -94,7 +94,7 @@ const sumUp = (...numbers) => {
   for (const num of numbers) {
     sum += validateNumber(num);
   }
-  return sum;
+  resultHandler(sum);
 };
 
 const subtractUp = function() {
@@ -103,8 +103,12 @@ const subtractUp = function() {
     sum -= num;
   }
   return sum;
-}
+};
 
-console.log(sumUp(1, 5, "blah", -3, 6, 10)); // ignores the string
-console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+const showResult = (result) => {
+  alert('The result after adding all numbers is: ' + result);
+};
+
+console.log(sumUp(showResult, 5, "blah", -3, 6, 10));
+console.log(sumUp(showResult, 5, 10, -3, 6, 10, 25, 88));
 console.log(subtractUp(1, 10, 15, 20));
