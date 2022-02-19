82d42306a8087dc4353c4f86a1513c3f0bab8263
pogopolice <pogopolice@gmail.com>
2022-02-19 08:21:35 -0500

    use the bind method in the showResults function

--- app.js
+++ app.js
@@ -86,29 +86,58 @@ startGameBtn.addEventListener('click', () => {
 
 // not related to game
 
-const sumUp = (resultHandler, ...numbers) => {
+const combine = (resultHandler, operation, ...numbers) => {
   const validateNumber = (number) => {
     return isNaN(number) ? 0 : number;
   };
   let sum = 0;
   for (const num of numbers) {
-    sum += validateNumber(num);
+    if (operation === 'ADD') {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
   }
   resultHandler(sum);
 };
 
-const subtractUp = function() {
-  let sum = 0;
-  for (const num of arguments) {  // dont use that
-    sum -= num;
-  }
-  return sum;
+// const subtractUp = function(resultHandler, ...numbers) {
+  // let sum = 0;
+  // for (const num of numbers) {
+    // sum -= num;
+  // }
+  // resultHandler(sum);
+// };
+
+const showResult = (messageText, result) => {
+  alert(messageText + ' ' + result);
 };
 
-const showResult = (result) => {
-  alert('The result after adding all numbers is: ' + result);
-};
-
-console.log(sumUp(showResult, 5, "blah", -3, 6, 10));
-console.log(sumUp(showResult, 5, 10, -3, 6, 10, 25, 88));
-console.log(subtractUp(1, 10, 15, 20));
+combine(
+  showResult.bind(this, 'The result after adding all numbers is: '),
+  'ADD',
+  5,
+  'blah',
+  -3,
+  6,
+  10
+);
+combine(
+  showResult.bind(this, 'The result after adding all numbers is: '),
+  'ADD',
+  5,
+  10,
+  -3,
+  6,
+  10,
+  25,
+  88
+);
+combine(
+  showResult.bind(this, 'The result after subtracting all numbers is: '),
+  'SUBTRACT',
+  1,
+  10,
+  15,
+  20
+);
